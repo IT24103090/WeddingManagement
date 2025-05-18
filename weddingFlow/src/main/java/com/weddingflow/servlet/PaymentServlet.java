1d053705390df273c9013913c9df5e43b6ad7ff3
IT24102109 <it24102109@my.sliit.lk>
2025-05-18 10:43:55 +0530

    Added attributes and usrname not found condition to PayemntServlet.java

--- weddingFlow/src/main/java/com/weddingflow/servlet/PaymentServlet.java
+++ weddingFlow/src/main/java/com/weddingflow/servlet/PaymentServlet.java
@@ -1,4 +1,20 @@
 package com.weddingflow.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class PaymentServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = (String) request.getSession().getAttribute("username");
+        String cardNumber = request.getParameter("cardNumber");
+        String expiryDate = request.getParameter("expiryDate");
+        String cvv = request.getParameter("cvv");
+
+        if (username == null) {
+            request.setAttribute("error", "You must be logged in to make a payment");
+            request.getRequestDispatcher("payment.jsp").forward(request, response);
+            return;
+        }
 }
